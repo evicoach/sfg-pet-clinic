d237b619e406d364d85e08d3ff46267dcef2fb8e
Evi Owhotemu <evicoach@gmail.com>
2022-01-12 08:17:49 +0100

    Adds model pojos

---
+++ src/main/java/com/columnhack/sfgpetclinic/model/Pet.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.columnhack.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author evi
+ */
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
