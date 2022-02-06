9d411764d58b68b5ddaefe9f5773c9811b2fa8de
Evi Owhotemu <evicoach@gmail.com>
2022-02-06 09:40:53 +0100

    Upgraded to JUnit 5. closes #4

--- pet-clinic-web/src/test/java/com/columnhack/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/com/columnhack/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -1,7 +1,16 @@
 package com.columnhack.sfgpetclinic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class SfgPetClinicApplicationTest {
 
+    @Test
+    public void contextLoad(){
+
+    }
 }
