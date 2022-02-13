ace5f751caa2d02a323be2150a70e1f22b38ca37
Evi Owhotemu <evicoach@gmail.com>
2022-02-13 07:41:24 +0100

    Added MockMvc test for owner controller. closes #54

--- pet-clinic-data/src/main/java/com/columnhack/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/columnhack/sfgpetclinic/model/PetType.java
@@ -5,8 +5,6 @@
  */
 package com.columnhack.sfgpetclinic.model;
 
-import lombok.*;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -15,14 +13,48 @@ import javax.persistence.Table;
  *
  * @author evi
  */
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pet_type")
 public class PetType  extends BaseEntity{
     @Column(name = "name")
     private String name;
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public PetType() {
+    }
+
+    public static PetTypeBuilder builder() {
+        return new PetTypeBuilder();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static class PetTypeBuilder {
+        private String name;
+
+        PetTypeBuilder() {
+        }
+
+        public PetTypeBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PetType build() {
+            return new PetType(name);
+        }
+
+        public String toString() {
+            return "PetType.PetTypeBuilder(name=" + this.name + ")";
+        }
+    }
 }
