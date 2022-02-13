0aca94895526fd9f6ed02ca007a645a6c2b92522
Evi Owhotemu <evicoach@gmail.com>
2022-02-13 06:25:49 +0100

    Creating test for controllers

---
+++ pet-clinic-web/src/test/java/com/columnhack/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,21 @@
+package com.columnhack.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listOwners() {
+    }
+
+    @Test
+    void findOwners() {
+    }
+}
